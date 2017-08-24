503f448907a0319cf445337df9a581d85c4c5c1f
Osman Ochoa <ochoaosman@gmail.com>
2017-08-23 21:26:15 -0400

    Setter and Getters

--- index.js
+++ index.js
@@ -1,30 +1,80 @@
 console.log('vehicle');
 
 class Engine {
-  constructor(cylinder,piston,) {
-    this.cylinder = cylinder;
-    this.heater = heater; 	
-    this.hood = hood;
-    this.horn = horn;	
-    this.ignition = ignition; 	
-    this.alternator = alternator;
-  }
+    constructor(cylinder, piston, ) {
+        this.cylinder = cylinder;
+        this.heater = heater;
+        this.hood = hood;
+        this.horn = horn;
+        this.ignition = ignition;
+        this.alternator = alternator;
+    }
 
-  Send_current(){
-    console.log(`send current do ${this.alternator}`)
-  }
+    /** Stters and Getters **/
+    get cylinder() {
+        return this.cylinder;
+    }
 
-   ingnition(){
-    console.log(`${this.ingnition}`)
-  }
+    set cylinder(value) {
+        this.cylinder = value;
+    }
 
-  cylinders() {
-    console.log(`${this.cylinder} cylinders engine`)
-  }
+    get heater() {
+        return this.heater;
+    }
 
-  start() {
-    console.log(`The engine has started!`);
-  }
+    set heater(value) {
+        this.heater = value;
+    }
+
+    get hood() {
+        return this.hood;
+    }
+
+    set hood(value) {
+        this.hood = value;
+    }
+
+    get horn() {
+        return this.horn;
+    }
+
+    set horn(value) {
+        this.horn = value;
+    }
+
+    get ignition() {
+        return this.horn;
+    }
+
+    set ignition(value) {
+        this.ignition = value;
+    }
+
+    get alternator() {
+        return this.alternator;
+    }
+
+    set alternator(value) {
+        this.alternator = value;
+    }
+
+    /** Function the Engine **/
+    Send_current() {
+        console.log(`send current do ${this.alternator}`)
+    }
+
+    ingnition() {
+        console.log(`${this.ingnition}`)
+    }
+
+    cylinders() {
+        console.log(`${this.cylinder} cylinders engine`)
+    }
+
+    start() {
+        console.log(`The engine has started!`);
+    }
 }
 
 const v8 = new Engine(8);
