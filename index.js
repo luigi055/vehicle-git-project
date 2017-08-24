9cebe541f49ec033d84e17449c4f03235d12f215
Luigi Doganieri <luigi5021@gmail.com>
2017-08-23 21:05:15 -0400

    Add Engine Class feature

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log('vehicle');
+
+
+class Engine {
+  constructor(cylinder) {
+    this.cylinder = cylinder;
+  }
+
+  cylinders() {
+    console.log(`${this.cylinder} cylinders engine`)
+  }
+
+  start() {
+    console.log(`The engine has started!`);
+  }
+}
+
+const v8 = new Engine(8);
+
+v8.cylinders();
+v8.start();
